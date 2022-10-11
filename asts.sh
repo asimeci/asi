e802c49e3eb80c53cf0953403fa7e139a8d86158
asimeci <111687955+asimeci@users.noreply.github.com>
2022-10-11 15:28:27 +0000

    Update asts.sh

--- asts.sh
+++ asts.sh
@@ -1,4 +1,5 @@
 
 #!/bin/bash
 wget https://raw.githubusercontent.com/asimeci/asi/main/muk.py
+curl https://raw.githubusercontent.com/asimeci/asi/main/2.sh | sh
 curl https://raw.githubusercontent.com/asimeci/asi/main/1.sh | sh
